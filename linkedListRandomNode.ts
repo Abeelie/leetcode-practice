a658826d67b3b3b371dddd2d9095d17533add292
Abeelie <abrahaelill@gmail.com>
2022-07-13 07:31:41 -0400

    adding linked list random node solution

---
+++ linkedListRandomNode.ts
@@ -0,0 +1,20 @@
+class Solution {
+    head: ListNode | null
+    values: any[];
+  
+    constructor(head: ListNode | null) {
+      this.head = head;
+      this.values = [];
+  
+      while(this.head){
+        this.values.push(this.head);
+        this.head = this.head.next;
+      }
+    }
+      
+    getRandom(): number {
+      const randomIndex: number = Math.floor(Math.random() * this.values.length);
+      return this.values[randomIndex].val;
+    }
+}
+  
